65293d5daa310aa9dc23e3faa581b25e825eb05c
Alex Kunin <abk170@psu.edu>
2018-08-10 12:32:39 -0400

    load by hash

--- src/fileio.jl
+++ src/fileio.jl
@@ -1,6 +1,4 @@
-# Methods for saving distributions to disk
 
-# more stringent check than ==
 function isequal(P::AbstractBinaryVectorDistribution, Q::AbstractBinaryVectorDistribution)
     typeof(P) == typeof(Q) || return false
     for (fp,fq) in zip(fieldnames(typeof(P)),fieldnames(typeof(Q)))
@@ -17,8 +15,6 @@ function hash(P::AbstractBinaryVectorDistribution, h::UInt)
     return _h
 end
 
-
-
 ################################################################################
 #### File IO
 ################################################################################
@@ -28,13 +24,13 @@ end
 Save the given distribution to disk using the `JLD` package. Returns the full path to the
 saved file.
 
-Default `filename` is `(typeof(P)) * "." * (hash(P)) * ".jld"`. Default `dir` is
+Default `filename` is `string(hash(P))`. Default `dir` is
 `Pkg.dir(BinaryVectorProbability)/saved`.
 
 If `dir` doesn't exist, will use `mkpath`. If file exists, contents will be overwritten.
 """
 function savedistribution(P::AbstractBinaryVectorDistribution;
-    filename="$(typeof(P)).$(hash(P))",
+    filename=string(hash(P)),
     dir=joinpath(Pkg.dir("BinaryVectorProbability"), "saved"))
 
     # _dir = abspath(dir)
@@ -61,3 +57,4 @@ function loaddistribution(filename; dir=joinpath(Pkg.dir("BinaryVectorProbabilit
     _fn = basename(_fn)
     return load(joinpath(dir, _fn), "P")
 end
+loaddistribution(h::UInt; dir=joinpath(Pkg.dir("BinaryVectorProbability"),"saved")) = loadstimulus(string(h), dir=dir)
